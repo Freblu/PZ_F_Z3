6798f722a9909a7fe67cc59f25e00032b1785dc6
Freblu <natanku.f@gmail.com>
2024-11-16 16:02:37 +0100

    Rozbudowanie Sort i dodanie Merge
    Rozbudowano metode sortowanie Sort oraz dodano metode łączenia posortowanych tablic

--- PZ_F_Z3/MergeSort.cpp
+++ PZ_F_Z3/MergeSort.cpp
@@ -17,17 +17,38 @@ MergeSort::~MergeSort()
 	delete dane; 
 };
 
-
-void MergeSort::Sort()
+void MergeSort::Merge(int p1[], int lr, int p2[], int pr)
 {
-	int p1 = pivot - lew + 1;
-	int p2 = pra - pivot;
+	int* tab = new int[roz];
+
+
+}
+
+void MergeSort::Sort(int tab[], int l, int p)
+{
+	if (l >= p)
+	{
+		return;
+	}
+	int pivot = l + (p - l) / 2;
+	Sort(tab, l, pivot);
+	Sort(tab, pivot + 1, p);
+	
+	int* p1 = new int[pivot - l + 1];
+	int* p2 = new int[p - pivot];
+
+	for (int i = 0; i <= pivot - lew; i++)
+	{
+		p1[i] = tab[l + i];
+	}
+
+	for (int i = 0; i <= p - pivot; i++)
+	{
+		p2[i] = tab[pivot + 1 + i];
+	}
+	Merge(p1, pivot - l + 1, p2, p - pivot);
 };
 
-void MergeSort::Mk_Pivot()
-{
-	pivot = pra + (pra - lew) / 2;
-};
 
 void MergeSort::Wypisz()
 {
--- PZ_F_Z3/MergeSort.h
+++ PZ_F_Z3/MergeSort.h
@@ -6,7 +6,6 @@ using namespace std;
 class MergeSort
 {
 private:
-	int pivot = 0;
 	int roz;
 	int* dane;
 	int lew;
@@ -15,8 +14,8 @@ public:
 
 	MergeSort(int tab[], int r, int p, int k);
 	~MergeSort();
-	void Sort();
-	void Mk_Pivot();
+	void Merge(int p1[], int lr, int p2[], int pr);
+	void Sort(int tab[], int l, int p);
 	void Wypisz();
 };
 

77e98f467d43591742f320472749a7c254fbf961
Freblu <natanku.f@gmail.com>
2024-11-23 20:08:33 +0100

    Dodano test ujemnej tablicy

--- MS_T/TestMergeSort.cpp
+++ MS_T/TestMergeSort.cpp
@@ -70,3 +70,27 @@ TEST_F(MergeSortTest, RandTest) {
     }
     MergeSortTest::TearDown();
 }
+
+TEST_F(MergeSortTest, NegativeTest) {
+    int tab[6];
+    for (int i = 0; i < 5; ++i) {
+        tab[i] = -abs(rand());
+    }
+    roz = sizeof(tab) / sizeof(tab[0]);
+    MergeSortTest::SetUp(tab, roz);
+
+    int p = 0;
+    int n = 0;
+    bool t;
+    for (int i = 0; i < roz; ++i) {
+        testTab[i] = p;
+        i++;
+        testTab[i] = n;
+        if (p <= n)
+            t = true;
+        else
+            t = false;
+        EXPECT_TRUE(t);
+    }
+    MergeSortTest::TearDown();
+}
